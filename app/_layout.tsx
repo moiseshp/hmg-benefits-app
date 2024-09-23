1672e92c9e77e0bfeb77d5fc3dbcd934faac7b9e
moiseshp <moiseseduardo.hp@gmail.com>
2024-09-22 22:15:14 -0500

    feat: auth integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import { Stack } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-// import { AuthProvider, useAuth } from '../context/AuthContext';
+import { SessionProvider } from '@/providers/session-provider';
+import { Slot } from 'expo-router';
 
-export default function RootLayout() {
+export default function Root() {
   return (
-    // <AuthProvider>
-    <SafeAreaView className="flex-1">
-      <Stack screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="(auth)/login" options={{ title: 'Login' }} />
-        <Stack.Screen name="(auth)/signup" options={{ title: 'Signup' }} /> */}
-        <Stack.Screen name="(app)/home" options={{ title: 'Home' }} />
-      </Stack>
-    </SafeAreaView>
-    // </AuthProvider>
+    <SessionProvider>
+      <Slot />
+    </SessionProvider>
   );
 }
